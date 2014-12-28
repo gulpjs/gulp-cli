b68c361ad4da8aba69350ccf2e5c0d5719f4a12b
Heikki Ylönen <heikki74@gmail.com>
2014-12-28 19:31:58 +0200

    Update: Improve tests for Windows

--- test/flags-task-simple.js
+++ test/flags-task-simple.js
@@ -6,12 +6,12 @@ var code = require('code');
 var fs = require('fs');
 var child = require('child_process');
 
-var output = fs.readFileSync(__dirname + '/expected/flags-tasks-simple.txt', 'utf8');
+var output = fs.readFileSync(__dirname + '/expected/flags-tasks-simple.txt', 'utf8').replace(/\r\n/g, '\n');
 
 lab.experiment('flag: --tasks-simple', function () {
 
   lab.test('prints the task list', function (done) {
-    child.exec(__dirname + '/../index.js --tasks-simple --cwd ./test', function(err, stdout) {
+    child.exec('node ' + __dirname + '/../index.js --tasks-simple --cwd ./test', function(err, stdout) {
       code.expect(stdout).to.equal(output);
       done(err);
     });
