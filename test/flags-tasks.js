2d8c37736c114020a87c2e67198d68005006f9d3
Heikki Ylönen <heikki74@gmail.com>
2014-12-28 20:16:48 +0200

    convert windows path separators

--- test/flags-tasks.js
+++ test/flags-tasks.js
@@ -9,6 +9,7 @@ lab.experiment('flag: --tasks', function () {
 
   lab.test('prints the task list', function (done) {
     child.exec('node ' + __dirname + '/../index.js --tasks --cwd ./test', function(err, stdout) {
+      stdout = stdout.replace(/\\/g, '/');
       code.expect(stdout).to.contain('/gulp-cli/test');
       code.expect(stdout).to.contain('├── test1');
       code.expect(stdout).to.contain('├── test2');
