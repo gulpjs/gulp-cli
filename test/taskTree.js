194f66720892adfcf558380b6a5c65ab4613c13f
Blaine Bublitz <blaine@iceddev.com>
2014-12-27 22:57:38 -0700

    add task tree test from main module

---
+++ test/taskTree.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var lab = exports.lab = require('lab').script();
+var code = require('code');
+
+var taskTree = require('../lib/taskTree');
+
+lab.experiment('taskTree()', function() {
+
+  lab.test('forms a tree properly', function(done) {
+    code.expect(taskTree).to.exist(); // lol shutup jshint
+
+    var tasks = {
+      test: {
+        dep: ['abc', 'def']
+      },
+      abc: {
+        dep: ['def']
+      },
+      def: {
+        dep: []
+      }
+    };
+
+    var expectTree = {
+      nodes: [{
+        label: 'test',
+        nodes: ['abc', 'def']
+      }, {
+        label: 'abc',
+        nodes: ['def']
+      }, {
+        label: 'def',
+        nodes: []
+      }]
+    };
+
+    code.expect(taskTree(tasks)).to.deep.equal(expectTree);
+    done();
+  });
+});
