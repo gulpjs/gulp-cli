ab8609724b170c220f81b6b152df4246742b6009
Heikki Ylönen <heikki74@gmail.com>
2015-02-22 10:47:33 -0400

    fix autocompletion
    closes #15

---
+++ test/completion.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var lab = exports.lab = require('lab').script();
+var code = require('code');
+
+var child = require('child_process');
+
+lab.experiment('flag: --completion', function () {
+
+  ['bash', 'fish', 'powershell', 'zsh'].forEach(function(type) {
+    lab.test('returns completion script for ' + type, function (done) {
+      child.exec('node ' + __dirname + '/../bin/gulp.js --completion=' + type, function(err, stdout) {
+        code.expect(stdout).to.contain('gulp --completion=' + type);
+        done(err);
+      });
+    });
+  });
+
+  lab.test('shows error message for unknown completion type', function (done) {
+    child.exec('node ' + __dirname + '/../bin/gulp.js --completion=unknown', function(err, stdout) {
+      code.expect(stdout).to.contain('rules for \'unknown\' not found');
+      done();
+    });
+  });
+
+});
