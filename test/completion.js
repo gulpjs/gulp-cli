5376c22414517b77fe2e15ea97ff258102473b44
Blaine Bublitz <blaine@iceddev.com>
2015-02-25 12:57:14 -0700

    make things work with babel change

--- test/completion.js
+++ test/completion.js
@@ -5,10 +5,10 @@ var code = require('code');
 
 var child = require('child_process');
 
-lab.experiment('flag: --completion', function () {
+lab.experiment('flag: --completion', function() {
 
   ['bash', 'fish', 'powershell', 'zsh'].forEach(function(type) {
-    lab.test('returns completion script for ' + type, function (done) {
+    lab.test('returns completion script for ' + type, function(done) {
       child.exec('node ' + __dirname + '/../bin/gulp.js --completion=' + type, function(err, stdout) {
         code.expect(stdout).to.contain('gulp --completion=' + type);
         done(err);
@@ -16,7 +16,7 @@ lab.experiment('flag: --completion', function () {
     });
   });
 
-  lab.test('shows error message for unknown completion type', function (done) {
+  lab.test('shows error message for unknown completion type', function(done) {
     child.exec('node ' + __dirname + '/../bin/gulp.js --completion=unknown', function(err, stdout) {
       code.expect(stdout).to.contain('rules for \'unknown\' not found');
       done();
--- test/flags-tasks.js
+++ test/flags-tasks.js
@@ -8,7 +8,7 @@ var child = require('child_process');
 lab.experiment('flag: --tasks', function() {
 
   lab.test('prints the task list', function(done) {
-    child.exec('node ' + __dirname + '/../bin/gulp.js --tasks --cwd ./test', function(err, stdout) {
+    child.exec('node ' + __dirname + '/../bin/gulp.js --tasks --cwd ./test/fixtures', function(err, stdout) {
       code.expect(stdout).to.contain('Tasks for');
       stdout = stdout.replace(/\\/g, '/').split('Tasks for')[1].split('\n');
       code.expect(stdout[0]).to.contain('/gulp-cli/test');
