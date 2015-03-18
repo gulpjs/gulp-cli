c559f798b9e7e4a791a64eb5ddf6d72ba5944623
louisremi <lrbabe@gmail.com>
2015-03-18 14:11:12 +0100

    fix #19 Support description + unit tests

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -3,9 +3,11 @@
 var gulp = require('gulp');
 
 function noop(){}
+function described(){}
+described.description = 'description';
 
 gulp.task('test1', noop);
-gulp.task('test2', noop);
-gulp.task('test3', noop);
+gulp.task('test2', ['test1'], noop);
+gulp.task('test3', described);
 
 gulp.task('default', noop);
