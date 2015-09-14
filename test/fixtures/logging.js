d20e11a624d3fac0f72853351dbf34330c6ba9a8
Kahlil Lechelt <hello@kahlil.info>
2015-09-14 17:26:25 +0200

    remove unecessary variable

--- test/fixtures/logging.js
+++ test/fixtures/logging.js
@@ -3,8 +3,7 @@ var yargs = require('yargs');
 var toConsole = require('../../lib/shared/log/toConsole');
 var cliOptions = require('../../lib/shared/cliOptions');
 
-var usage = '';
-var parser = yargs.usage(usage, cliOptions);
+var parser = yargs.usage('', cliOptions);
 var opts = parser.argv;
 
 toConsole(log, opts);
