6af236ea8c72ee1921d572aadb99d9dfb4f02d47
Romanx <mcauliffe.alex@gmail.com>
2015-10-20 20:00:28 +0100

    Added placeholder for require opts

---
+++ test/flags-require.js
@@ -0,0 +1,11 @@
+var lab = exports.lab = require('lab').script();
+var code = require('code');
+
+var fs = require('fs');
+var child = require('child_process');
+
+lab.experiment('flag: --require', function() {
+
+  lab.test('requires module before running gulpfile');
+  
+});
