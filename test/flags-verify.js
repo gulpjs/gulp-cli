b45beebbdaf33a73421469e2b467a24688a07e0c
Romanx <mcauliffe.alex@gmail.com>
2015-10-21 19:45:37 +0100

    Package path should no longer be relative
    See cwd change

--- test/flags-verify.js
+++ test/flags-verify.js
@@ -7,7 +7,7 @@ var child = require('child_process');
 lab.experiment('flag: --verify', function() {
 
   lab.test('dependencies with invalid dependency', function(done) {
-    child.exec('node ' + __dirname + '/../bin/gulp.js --verify "packages/invalid-package.json" --cwd "./test/fixtures/packages/"', function(err, stdout) {
+    child.exec('node ' + __dirname + '/../bin/gulp.js --verify "invalid-package.json" --cwd "./test/fixtures/packages/"', function(err, stdout) {
       // Ignore err as the verify failure is considered a error state.
       stdout = stdout.replace(/\\/g, '/').split('\n');
       code.expect(stdout[1]).to.contain('Blacklisted plugins found in this project:');
@@ -17,7 +17,7 @@ lab.experiment('flag: --verify', function() {
   });
 
   lab.test('dependencies with valid dependency', function(done) {
-    child.exec('node ' + __dirname + '/../bin/gulp.js --verify "packages/valid-package.json" --cwd "./test/fixtures/packages/"', function(err, stdout) {
+    child.exec('node ' + __dirname + '/../bin/gulp.js --verify "valid-package.json" --cwd "./test/fixtures/packages/"', function(err, stdout) {
       stdout = stdout.replace(/\\/g, '/').split('\n');
       code.expect(stdout[1]).to.contain(' There are no blacklisted plugins in this project');
       done(err);
@@ -25,8 +25,7 @@ lab.experiment('flag: --verify', function() {
   });
 
   lab.test('default args with invalid dependency', function(done) {
-    child.exec('node ' + __dirname + '/../bin/gulp.js --verify "packages/package.json" --cwd ./test/fixtures/packages/', function(err, stdout) {
-      console.log(stdout);
+    child.exec('node ' + __dirname + '/../bin/gulp.js --verify "package.json" --cwd ./test/fixtures/packages/', function(err, stdout) {
       // Ignore err as the verify failure is considered a error state.
       stdout = stdout.replace(/\\/g, '/').split('\n');
       code.expect(stdout[1]).to.contain('Blacklisted plugins found in this project:');
