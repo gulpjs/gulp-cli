74c5b1d9c1d8de5651ff82056f3c05feac9aec0c
Blaine Bublitz <blaine@iceddev.com>
2015-11-18 13:01:22 -0700

    make CLI support new undertaker tree return value

--- test/flags-tasks-json.js
+++ test/flags-tasks-json.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var fs = require('fs-extra');
+var path = require('path');
+var tildify = require('tildify');
 var lab = exports.lab = require('lab').script();
 var code = require('code');
 
@@ -9,11 +11,14 @@ var output = require('./expected/flags-tasks-json.json')
 
 lab.experiment('flag: --tasks-json', function() {
 
+  var outputString = JSON.stringify(output).replace(/{{path}}/, tildify(path.join(__dirname, 'fixtures/gulpfile.js')));
+  var expected = JSON.parse(outputString);
+
   lab.test('prints the task list with no args', function(done) {
     child.exec('node ' + __dirname + '/../bin/gulp.js --tasks-json --gulpfile "./test/fixtures/gulpfile.js" ', function(err, stdout) {
       stdout = stdout.replace(/\\/g, '/').split('\n');
       var parsedJson = JSON.parse(stdout[1]);
-      code.expect(parsedJson).to.deep.equal(output);
+      code.expect(parsedJson).to.deep.equal(expected);
       done(err);
     });
   });
@@ -27,7 +32,7 @@ lab.experiment('flag: --tasks-json', function() {
       child.exec('node ' + __dirname + '/../bin/gulp.js --tasks-json "../output/tasks.json" --gulpfile "./test/fixtures/gulpfile.js" ', function(err, stdout) {
         var file = fs.readFileSync(__dirname + '/output/tasks.json', 'utf8')
         var parsedJson = JSON.parse(file);
-        code.expect(parsedJson).to.deep.equal(output);
+        code.expect(parsedJson).to.deep.equal(expected);
         fs.removeSync(__dirname + '/output/')
         done(err);
       });
