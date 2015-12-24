3e0fc18b41885fae7934ed07f6247cda91b8bea2
sttk <t110000508260@yahoo.co.jp>
2015-12-24 23:20:49 +0900

    New: Add --depth flag to pair with --tasks flag

---
+++ test/fixtures/flags-tasks-with-desc.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp');
+
+function clean() {}
+clean.description = 'Delete dist folder';
+gulp.task(clean);
+
+gulp.task('scripts', scripts);
+function scripts() {};
+scripts.description = 'Bundles JavaScript';
+
+var styles = function() {};
+gulp.task('styles', styles);
+gulp.task('styles').description = 'Compiles and bundles CSS';
+
+var build = gulp.series('clean', 'scripts', 'styles');
+build.description = 'Build all the things!';
+build.flags = {
+  '--dev': 'un-minified',
+  '--production': 'compressed into single bundle',
+  '': 'dummy-empty-string',
+};
+gulp.task('build', build);
+
+gulp.task('serve', serve);
+function serve() {};
+serve.description = 'Serves files reloading';
+serve.flags = {
+  '--lr': 'with live reloading',
+};
+
+function watch() {};
+gulp.task('watch', watch);
+watch.description = 'Watch files and build on change';
+
+gulp.task('default', gulp.series('build', 'watch'));
+gulp.task('default').description = 'Build and watch for changes';
