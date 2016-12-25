445395c6c338c67a28d201478de2ef7ebb50ffe3
sttk <t110000508260@yahoo.co.jp>
2016-12-25 19:48:35 +0900

    Enables to measure coverage of executions on child processes

--- test/logging.js
+++ test/logging.js
@@ -2,11 +2,29 @@
 
 var expect = require('expect');
 var child = require('child_process');
+var path = require('path');
+
+// jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+var isIstanbul = process.env.running_under_istanbul;
+// jscs:enable
+
+var cmd;
+if (isIstanbul) {
+  cmd = path.resolve(__dirname, '../node_modules/.bin/istanbul') +
+    ' cover --root ' + path.dirname(__dirname) +
+    ' ' + __dirname + '/fixtures/logging.js' +
+    ' --dir ' + path.dirname(__dirname) + '/coverage/logging' +
+    ' --print none' +
+    ' --';
+} else {
+  cmd = 'node ' + __dirname + '/fixtures/logging.js';
+}
+
 
 describe('logging', function() {
 
   it('log-level flag for debug: -LLLL', function(done) {
-    child.exec('node ' + __dirname + '/fixtures/logging.js -LLLL', cb);
+    child.exec(cmd + ' -LLLL', cb);
 
     function cb(err, stdout, stderr) {
       stdout = stdout.replace(/\\/g, '/').split('\n');
@@ -19,7 +37,7 @@ describe('logging', function() {
   });
 
   it('no log-level flag: defaults to -LLL', function(done) {
-    child.exec('node ' + __dirname + '/fixtures/logging.js', cb);
+    child.exec(cmd, cb);
 
     function cb(err, stdout, stderr) {
       stdout = stdout.replace(/\\/g, '/').split('\n');
@@ -31,7 +49,7 @@ describe('logging', function() {
   });
 
   it('log-level flag for info: -LLL', function(done) {
-    child.exec('node ' + __dirname + '/fixtures/logging.js -LLL', cb);
+    child.exec(cmd + ' -LLL', cb);
 
     function cb(err, stdout, stderr) {
       stdout = stdout.replace(/\\/g, '/').split('\n');
@@ -43,7 +61,7 @@ describe('logging', function() {
   });
 
   it('log-level flag for warn: -LL', function(done) {
-    child.exec('node ' + __dirname + '/fixtures/logging.js -LL', cb);
+    child.exec(cmd + ' -LL', cb);
 
     function cb(err, stdout, stderr) {
       stdout = stdout.replace(/\\/g, '/').split('\n');
@@ -54,7 +72,7 @@ describe('logging', function() {
   });
 
   it('log-level flag for error: -L', function(done) {
-    child.exec('node ' + __dirname + '/fixtures/logging.js -L', cb);
+    child.exec(cmd + ' -L', cb);
 
     function cb(err, stdout, stderr) {
       expect(stderr).toMatch('test error');
