be9d25a98429e0760973147bec22e5126245a4f4
sttk <sttk.xslet@gmail.com>
2019-03-14 00:23:55 +0900

    Fix: Allow gulpfiles specified by .gulp.* to register a loader (fixes #181)

--- index.js
+++ index.js
@@ -85,23 +85,29 @@ cli.on('respawn', function(flags, child) {
   log.info('Respawned to PID:', pid);
 });
 
+
 function run() {
-  cli.launch({
+  cli.prepare({
     cwd: opts.cwd,
     configPath: opts.gulpfile,
     require: opts.require,
     completion: opts.completion,
-  }, handleArguments);
+  }, function(env) {
+
+    var cfgLoadOrder = ['home', 'cwd'];
+    var cfg = loadConfigFiles(env.configFiles['.gulp'], cfgLoadOrder);
+    opts = mergeConfigToCliFlags(opts, cfg);
+    env = mergeConfigToEnvFlags(env, cfg);
+    env.configProps = cfg;
+
+    cli.execute(env, handleArguments);
+  });
 }
 
 module.exports = run;
 
 // The actual logic
 function handleArguments(env) {
-  var cfgLoadOrder = ['home', 'cwd'];
-  var cfg = loadConfigFiles(env.configFiles['.gulp'], cfgLoadOrder);
-  opts = mergeConfigToCliFlags(opts, cfg);
-  env = mergeConfigToEnvFlags(env, cfg);
 
   // This translates the --continue flag in gulp
   // To the settle env variable for undertaker
@@ -178,5 +184,6 @@ function handleArguments(env) {
   }
 
   // Load and execute the CLI version
-  require(path.join(__dirname, '/lib/versioned/', range, '/'))(opts, env, cfg);
+  var versionedDir = path.join(__dirname, '/lib/versioned/', range, '/');
+  require(versionedDir)(opts, env, env.configProps);
 }
