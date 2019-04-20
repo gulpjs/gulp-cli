190aaeb5a7c3a8a7b0c422c00bc82122a2dbb683
Corey Farrell <git@cfware.com>
2019-04-20 13:21:05 -0400

    New: Honor displayName in ESM exports (closes gulpjs/gulp#2270) (#189)

--- test/fixtures/gulpfiles/gulpfile-exports.babel.js
+++ test/fixtures/gulpfiles/gulpfile-exports.babel.js
@@ -8,3 +8,15 @@ export function clean(){};
 export function build(){};
 export const string = 'no function';
 export const dist = gulp.series(clean, build);
+function p1(){}
+function p2(){}
+export const p = gulp.parallel(p1, p2);
+
+export const sTest = gulp.series(clean, build);
+sTest.displayName = 's-test';
+
+export const pTest = gulp.parallel(p1, p2);
+pTest.displayName = 'p-test';
+
+export function fTest(){};
+fTest.displayName = 'f-test';
