2e2ed57e035a9996eca29a07811f8cc3fbcea489
Blaine Bublitz <blaine.bublitz@gmail.com>
2024-03-23 19:22:08 -0700

    update messages

--- test/fixtures/config/theming/GULPLOG_DEPRECATED/.gulp.js
+++ test/fixtures/config/theming/GULPLOG_DEPRECATED/.gulp.js
@@ -1,9 +1,8 @@
-// TODO: Add the symbol to messages
 var messages = require('@gulpjs/messages');
 
 module.exports = {
   message: function (data) {
-    if (data.tag === Symbol.for('GULP_CLI_GULPLOG_DEPRECATED')) {
+    if (data.tag === messages.GULPLOG_DEPRECATED) {
       return 'GULPLOG V1 IS DEPRECATED';
     }
 
